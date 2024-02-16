d07c26f6a350fd717453d70145e416f83490fd98
AJ Slater <aj@slater.net>
2024-02-16 14:12:29 -0800

    update eslint to flat config. update deps. lint.

--- tests/test_track.py
+++ tests/test_track.py
@@ -13,18 +13,18 @@ class TestTrack:
 
     def test_video_track(self):
         """Test video track."""
-        data = json.loads(read("clean_tracks.json"))["tracks"][0]
+        data = json.loads(read("clean-tracks.json"))["tracks"][0]
         track = Track(data)
         assert str(track) == "Track #0: und - MPEG-4p10/AVC/h.264"
 
     def test_audio_track(self):
         """Test audio track."""
-        data = json.loads(read("clean_tracks.json"))["tracks"][1]
+        data = json.loads(read("clean-tracks.json"))["tracks"][1]
         track = Track(data)
         assert str(track) == "Track #2: eng - AC-3"
 
     def test_subtitle_track(self):
         """Test subtitile track."""
-        data = json.loads(read("clean_tracks.json"))["tracks"][2]
+        data = json.loads(read("clean-tracks.json"))["tracks"][2]
         track = Track(data)
         assert str(track) == "Track #5: eng - SubRip/SRT"
