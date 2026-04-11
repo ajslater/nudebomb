0589b1ad0db886083f5a124fd85edc178df5c8b0
AJ Slater <aj@slater.net>
2026-04-10 21:02:43 -0700

    add ice cream debugger

--- nudebomb/__init__.py
+++ nudebomb/__init__.py
@@ -1,1 +1,9 @@
 """Nudebomb."""
+
+from os import environ
+
+PROGRAM_NAME = "nudebomb"
+if environ.get("PYTHONDEVMODE"):
+    from icecream import install  # pyright: ignore[reportPrivateImportUsage]
+
+    install()
